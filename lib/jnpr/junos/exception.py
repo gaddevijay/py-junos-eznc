75d06bd3a5cc733eb143bcf39cc6f276fee95150
Balasankar Rajaguru <balasank@juniper.net>
2022-05-08 19:14:25 +0530

    seperate consumer

--- lib/jnpr/junos/exception.py
+++ lib/jnpr/junos/exception.py
@@ -373,6 +373,7 @@ class OCTermProducer(RpcError):
     """
 
     def __init__(self, cmd, error, uuid=None):
+        self.error = error
         self.uuid = uuid
         RpcError.__init__(self, cmd=cmd, errs=error)
 
