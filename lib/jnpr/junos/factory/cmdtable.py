8b1aee184f7f1fadc4ee7cbbf4d9cc1ae339391e
Balasankar Rajaguru <balasank@juniper.net>
2024-01-11 13:02:24 +0530

    Skip SSH connection when RPC is timed out

--- lib/jnpr/junos/factory/cmdtable.py
+++ lib/jnpr/junos/factory/cmdtable.py
@@ -9,10 +9,10 @@ from inspect import isclass
 from jinja2 import Template
 
 # local
-from jnpr.junos.exception import RpcError
+from jnpr.junos.exception import RpcError, RpcTimeoutError
+from jnpr.junos.utils.start_shell import StartShell
 from jnpr.junos.factory.state_machine import StateMachine
 from jnpr.junos.factory.to_json import TableJSONEncoder
-from jnpr.junos.utils.start_shell import StartShell
 from lxml import etree
 
 # https://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
@@ -149,7 +149,11 @@ class CMDTable(object):
                         # </rpc-reply>
                         # hence need to do cleanup
                         self.data = self.data.replace("GOT: ", "")
-                except RpcError:
+                except RpcTimeoutError as exc:
+                    logger.error(f"RPC timedout, cmd: {self.GET_CMD}, target: {self.TARGET}, details: {exc}")
+                    raise exc
+                except RpcError as exc:
+                    logger.error(f"RPC errored falling to SSH, cmd: {self.GET_CMD}, target: {self.TARGET}, details: {exc}")
                     with StartShell(self.D) as ss:
                         ret = ss.run(
                             'cprod -A %s -c "%s"' % (self.TARGET, self.GET_CMD)
